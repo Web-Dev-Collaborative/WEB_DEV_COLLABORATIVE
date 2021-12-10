675f4725e3d352c2de378c435269c96d083bbbd8
Bryan C Guner <bryan.guner@gmail.com>
2021-12-10 22:20:41 +0000

    update

---
+++ bookmarks-v2.0/js/date.js
@@ -0,0 +1,18 @@
+var now = new Date();
+
+var days = new 
+Array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
+
+var months = new  
+Array('January','February','March','April','May','June','July','August','September','October','November','December');
+
+var date = ((now.getDate()<10) ? "0" : "")+ now.getDate();
+
+function fourdigits(number)	{
+	return (number < 1000) ? number + 1900 : number;
+}
+
+today =  days[now.getDay()] + ", " + date + " " + months[now.getMonth()] 
++ " " + (fourdigits(now.getYear()));
+
+document.write(today);
